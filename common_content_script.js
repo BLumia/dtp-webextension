2030e18b821da1aa164663a7506d41d5a2ad6523
Gary Wang <wzc782970009@gmail.com>
2020-06-09 23:08:05 +0800

    fix: should match all images

--- common_content_script.js
+++ common_content_script.js
@@ -1,13 +1,17 @@
 function getElementByXpath(path) {
-	return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+	return document.evaluate(path, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 }
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if (request.action == "requestPics") {
 		// TODO: check url, use different match rule to return data.
-		let a = getElementByXpath("//article[contains(@role,'article')][1]//a[contains(@href,'/photo/')]//img/@src");
-		console.log(a);
-		sendResponse({dateType: 'urlList', urlList: a});
+		let a = getElementByXpath("(//article[contains(@role,'article')])[1]//a[contains(@href,'/photo/')]//img/@src");
+		let arr = [];
+		for (i=0; i<a.snapshotLength; i++) {
+			arr.push(a.snapshotItem(i).textContent);
+		}
+// 		console.log(arr);
+		sendResponse({dateType: 'urlList', urlList: arr});
 	} else {
 		sendResponse({}); // Send nothing..
 	}
